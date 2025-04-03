0d4dd4e3da57710e7fd2176a43a84202bf978655
Phil <philippe.mm@hotmail.com>
2025-04-02 21:02:04 -0400

    Navbar progress

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/client/src', // Update alias if needed
+    },
+  },
 });
